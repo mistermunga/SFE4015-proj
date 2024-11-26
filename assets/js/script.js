944cbb434684f13d061d687767abb798bc1ba4b5
Ryan Munga <131756414+mistermunga@users.noreply.github.com>
2024-11-26 12:33:37 +0300

    modified script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,9 +13,6 @@ modalCloseOverlay.addEventListener('click', modalCloseFunc);
 modalCloseBtn.addEventListener('click', modalCloseFunc);
 
 
-
-
-
 // notification toast variables
 const notificationToast = document.querySelector('[data-toast]');
 const toastCloseBtn = document.querySelector('[data-toast-close]');
@@ -24,9 +21,6 @@ const toastCloseBtn = document.querySelector('[data-toast-close]');
 toastCloseBtn.addEventListener('click', function () {
   notificationToast.classList.add('closed');
 });
-
-
-
 
 
 // mobile menu variables
